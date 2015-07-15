12e6bc0979adf6e8cf1c749f4ca6a7655e6fd07e
Sandro Bonazzola <sbonazzo@redhat.com>
2015-07-15 11:42:31 +0200

    packaging: fix spec file
    add also automation to check if it builds.
    
    Change-Id: Icdedcb992f311cf34d4498b822cc63518fed3b83
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,5 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash -xe
+
+# remove any previous artifacts
+rm -rf output
+rm -f ./*tar.gz
 autoreconf -ivf
 ./configure
-find . -name "*.py" -exec pyflakes {} \+
-find . -name "*.py" -exec pep8 {} \+
+make clean
+
+make distcheck
+
+# create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -ts ./*.gz
+
+# install any build requirements
+yum-builddep output/*src.rpm
+
+# create the rpms
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    --rebuild output/*.src.rpm
