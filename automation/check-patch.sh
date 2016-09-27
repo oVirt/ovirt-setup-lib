6df4b6d8e41bf7547ccb62fde3d2750735eda97f
Yedidyah Bar David <didi@redhat.com>
2016-09-27 15:03:01 +0300

    core: Use python3 on fedora 23+
    Bug-Url: https://bugzilla.redhat.com/1379664
    Change-Id: I0feb545db10b3bc05cafa0b8a3af55fa96ab3b15
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,25 +1,3 @@
 #!/bin/bash -xe
 
-# remove any previous artifacts
-rm -rf output
-rm -f ./*tar.gz
-autoreconf -ivf
-./configure
-make clean
-
-make distcheck
-
-# create the src.rpm
-rpmbuild \
-    -D "_srcrpmdir $PWD/output" \
-    -D "_topmdir $PWD/rpmbuild" \
-    -ts ./*.gz
-
-# install any build requirements
-yum-builddep output/*src.rpm
-
-# create the rpms
-rpmbuild \
-    -D "_rpmdir $PWD/output" \
-    -D "_topmdir $PWD/rpmbuild" \
-    --rebuild output/*.src.rpm
+. automation/common.sh
