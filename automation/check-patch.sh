8feb8feeea97cbadd7d38883f7a8d4a0c8cf27d1
Sandro Bonazzola <sbonazzo@redhat.com>
2016-09-30 04:20:23 -0400

    Revert "core: Use python3 on fedora 23+"
    This reverts commit 6df4b6d8e41bf7547ccb62fde3d2750735eda97f.
    engine-setup has too many incompatibilities with python3.
    
    
    Change-Id: Ifef07fc7f9d7433f160b451b3b171173c66b893c
    Signed-Off-By: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,3 +1,25 @@
 #!/bin/bash -xe
 
-. automation/common.sh
+# remove any previous artifacts
+rm -rf output
+rm -f ./*tar.gz
+autoreconf -ivf
+./configure
+make clean
+
+make distcheck
+
+# create the src.rpm
+rpmbuild \
+    -D "_srcrpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    -ts ./*.gz
+
+# install any build requirements
+yum-builddep output/*src.rpm
+
+# create the rpms
+rpmbuild \
+    -D "_rpmdir $PWD/output" \
+    -D "_topmdir $PWD/rpmbuild" \
+    --rebuild output/*.src.rpm
