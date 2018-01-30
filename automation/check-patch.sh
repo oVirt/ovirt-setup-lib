57b0f441f0bbd7d1c387d3a9bc9fcf3fb78d619d
Sandro Bonazzola <sbonazzo@redhat.com>
2018-01-30 16:51:05 +0100

    automation: add coverage data
    added coverage data collection
    
    Change-Id: Ib38188a704cb8eace381fb7a3fe122a29562797d
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -3,9 +3,17 @@
 # remove any previous artifacts
 rm -rf output
 rm -f ./*tar.gz
+
+#for some reason looks like otopi is not getting installed by STD ci scripts
+yum -y install otopi
+
 autoreconf -ivf
 ./configure
 make clean
+export COVERAGE_PROCESS_START="${PWD}/automation/coverage.rc"
+export COVERAGE_FILE=$(mktemp -p $PWD .coverage.XXXXXX)
+export OTOPI_DEBUG=1
+export OTOPI_COVERAGE=1
 make distcheck
 
 # create the src.rpm
@@ -22,3 +30,9 @@ rpmbuild \
     -D "_rpmdir $PWD/output" \
     -D "_topmdir $PWD/rpmbuild" \
     --rebuild output/*.src.rpm
+
+unset COVERAGE_FILE
+coverage combine
+sed -i "s:ovirt-setup-lib-[0-9\.\-]*master/::g" .coverage
+coverage html -d exported-artifacts/coverage_html_report
+cp automation/index.html exported-artifacts/
