8eff98928e50f779cde3ed27bc36341389dbcea1
Sandro Bonazzola <sbonazzo@redhat.com>
2019-11-21 14:44:40 +0100

    build: post ovirt-setup-lib-1.3.0
    Change-Id: Ibd71c1ddfcbbbbf343d63748d2b56e82e0d79481
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 # remove any previous artifacts
 rm -rf output
@@ -15,6 +16,7 @@ make dist
 rpmbuild \
     -D "_srcrpmdir $PWD/output" \
     -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
     -ts ./*.gz
 
 # install any build requirements
@@ -24,6 +26,7 @@ yum-builddep output/*src.rpm
 rpmbuild \
     -D "_rpmdir $PWD/output" \
     -D "_topmdir $PWD/rpmbuild" \
+    -D "release_suffix ${SUFFIX}" \
     --rebuild output/*.src.rpm
 
 # Store any relevant artifacts in exported-artifacts for the ci system to
