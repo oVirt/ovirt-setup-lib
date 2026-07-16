a3120f5b98ef3656375b3ec25909725865eaa8d3
Jean-Louis Dupond <jean-louis@dupond.be>
2026-07-16 12:21:53 +0200

    build: update build flow
    - Update the way version tags are added during build.
      Now we can tag a commit in github, and the correct version string will
      be used for the builds.
    - Run the workflow on any tag, so it builds tags in branches also.
    - Drop copr
    - Remove the unused automation directory
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

---
+++ autogen.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Run this to generate configure and Makefile
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR=`pwd`
+(
+  cd $srcdir
+  die=0
+
+  (autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	  echo
+	  echo "You must have autoconf installed."
+	  echo "Download the appropriate package for your distribution,"
+	  echo "or see http://www.gnu.org/software/autoconf"
+	  die=1
+  }
+
+  (automake --version) < /dev/null > /dev/null 2>&1 || {
+	  echo
+	  die=1
+	  echo "You must have automake installed."
+	  echo "Download the appropriate package for your distribution,"
+	  echo "or see http://www.gnu.org/software/automake"
+  }
+
+  test $die = 1 && exit 1
+
+  test -f ovirt-setup-lib.spec.in || {
+	  echo "You must run this script in the top-level directory"
+	  exit 1
+  }
+
+  if test -z "$*"; then
+	  echo "I am going to run ./configure with no arguments - if you wish "
+	  echo "to pass any to it, please specify them on the $0 command line."
+  fi
+
+  autopoint
+  autoreconf -i
+  ./configure "$@"
+)
+
+if test "x$OBJ_DIR" != x; then
+    mkdir -p "$OBJ_DIR"
+    cd "$OBJ_DIR"
+fi
